3d37506dc78642641b1376e2d7a1d7334bd43162
Brad <brad901017@gmail.com>
2022-03-04 15:56:03 +0800

    class work

--- src/main/java/poker/brad/Hello.java
+++ src/main/java/poker/brad/Hello.java
@@ -3,7 +3,13 @@ package poker.brad;
 public class Hello {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        String[] name ={"Brad","Julia","Poker","Tom","Fish"};
+        int[] english ={60,55,80,70,90};
+        int[] math ={55,78,90,85,60};
+        for(int i=0;i<5;i++){
+        System.out.println(name[i]+" "+english[i]+" "+math[i]+" "+(english[i]+math[i])/2);
+        }
+
     }
 }
 
