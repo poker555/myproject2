ede7a61feeb61022cea286976f4a1b49b6fcbc6e
poker555 <brad901017@gmail.com>
2022-05-28 19:26:24 +0800

    Guess number

--- src/main/java/ui/GuessFrame.java
+++ src/main/java/ui/GuessFrame.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame
 {
@@ -13,17 +14,35 @@ public class GuessFrame extends JFrame
         setSize(600,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button = new JButton("Hi");
+        JButton button = new JButton("Guess");
+        JTextField number = new JTextField(8);
+        Random r = new Random();
+        int ans = r.nextInt(100)+1;
         JLabel label = new JLabel("ZZZZ");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                System.out.println("Hello!");
-                label.setText("Hello");
+                int num = Integer.parseInt(number.getText());
+                System.out.println(ans);
+                if (num>ans)
+                {
+                    System.out.println("smaller");
+                }
+                else if (num<ans)
+                {
+                    System.out.println("Bigger");
+                }
+                else
+                {
+                    System.out.println("bingo,the secret number is"+ans);
+                }
+
+
             }
         });
         setLayout(new FlowLayout());
+        add(number);
         add(button);
         add(label);
         setVisible(true);
