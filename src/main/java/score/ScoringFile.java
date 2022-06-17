d5247085d2db0479cea0be6e4711645d9fd892b7
poker555 <brad901017@gmail.com>
2022-06-17 16:09:07 +0800

    ScoringFile

---
+++ src/main/java/score/ScoringFile.java
@@ -0,0 +1,31 @@
+package score;
+
+import javax.imageio.IIOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ScoringFile
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            FileReader fileReader = new FileReader("score.txt");
+            int data = fileReader.read();
+            System.out.println(data);
+            data = fileReader.read();
+            System.out.println(data);
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("檔案讀取失敗");
+        }
+        catch (IOException e)
+        {
+            System.out.println("資料讀取失敗");
+        }
+        System.out.println("Testing");
+
+    }
+}
